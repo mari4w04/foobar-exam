08221f1522bd778825d6509c670f2642ad73543f
Maria <mari4w04@stud.kea.dk>
2018-06-14 11:19:10 +0200

    It's working

--- js/main.js
+++ js/main.js
@@ -1,46 +1,72 @@
 "use strict"
-window.addEventListener("load", gettingData);
-let data;
-//Getting data every 10 seconds
-setInterval(gettingData, 100000000);
 
+window.addEventListener("load", showAllData);
+
+function showAllData(){
+    gettingData();
+    //Getting data every 10 seconds
+    setInterval(gettingData, 100000);
+    getBeerData();
+};
+
+let data;
+let dashboardStorageInfo, dashboardQueueInfo, dashboardServingInfo, dashboardBeerLevel, dashboardBarInfo;
 let allOrders = 0; 
-let b1Customers = [], b2Customers = [], b3Customers = [], i;
+let b1Customers = [], b2Customers = [], b3Customers = [], beersServedIds = [], i;
 let mydata = {"timestamp":1528704419454,"bar":{"name":"FooBar","closingTime":"22:00:00"},"queue":[{"id":8,"startTime":1528704336575,"order":["Hoppily Ever After","Hoppily Ever After","Mowintime","Ruined Childhood"]},{"id":9,"startTime":1528704336575,"order":["Mowintime"]},{"id":10,"startTime":1528704396577,"order":["Hollaback Lager","Steampunk"]},{"id":11,"startTime":1528704396577,"order":["Mowintime","Mowintime"]}],"serving":[{"id":5,"startTime":1528704336575,"order":["Hollaback Lager","Hollaback Lager","Mowintime","Mowintime"]},{"id":6,"startTime":1528704336575,"order":["Hoppily Ever After","Hoppily Ever After","Steampunk"]},{"id":7,"startTime":1528704336575,"order":["Hoppily Ever After","Mowintime","Mowintime","Steampunk"]}],"bartenders":[{"name":"Peter","status":"WORKING","statusDetail":"pourBeer","usingTap":3,"servingCustomer":7},{"name":"Jonas","status":"WORKING","statusDetail":"pourBeer","usingTap":1,"servingCustomer":5},{"name":"Martin","status":"WORKING","statusDetail":"pourBeer","usingTap":2,"servingCustomer":6}],"taps":[{"id":0,"level":2300,"capacity":2500,"beer":"Hoppily Ever After","inUse":false},{"id":1,"level":2200,"capacity":2500,"beer":"Mowintime","inUse":true},{"id":2,"level":2400,"capacity":2500,"beer":"Steampunk","inUse":true},{"id":3,"level":2450,"capacity":2500,"beer":"Hoppily Ever After","inUse":true},{"id":4,"level":2450,"capacity":2500,"beer":"Ruined Childhood","inUse":false},{"id":5,"level":2350,"capacity":2500,"beer":"Hollaback Lager","inUse":false},{"id":6,"level":2400,"capacity":2500,"beer":"Mowintime","inUse":false}],"storage":[{"name":"El Hefe","amount":5},{"name":"Fairy Tale Ale","amount":5},{"name":"GitHop","amount":6},{"name":"Hollaback Lager","amount":4},{"name":"Hoppily Ever After","amount":6},{"name":"Mowintime","amount":6},{"name":"Row 26","amount":9},{"name":"Ruined Childhood","amount":5},{"name":"Sleighride","amount":2},{"name":"Steampunk","amount":8}]};
+
+function getBeerData(){
+    data = JSON.parse(FooBar.getData()); //mydata
+    //Setting beer info data in the template
+    showBeerTypeData(data.beertypes);
+};
 
 function gettingData(){
     
-    let dashboardBarInfo = document.querySelector(".bar-info");
-    let dashboardBartenderInfo = document.querySelector(".bartender-info");
-    let dashboardBeerLevel = document.querySelector(".beer-levels");
-    let dashboardQueueInfo = document.querySelector(".queue-info");
-    let dashboardServingInfo = document.querySelector(".serving-info");
-    let dashboardStorageInfo = document.querySelector(".storage-info");
-    let dashboardBeerInfo = document.querySelector(".beer-info");
-
-
-    dashboardBarInfo.innerHTML = "";
-    dashboardBartenderInfo.innerHTML = "";
-    dashboardBeerLevel.innerHTML = "";
-    dashboardQueueInfo.innerHTML = "";
-    dashboardServingInfo.innerHTML = "";
-    dashboardStorageInfo.innerHTML = "";
-    dashboardBeerInfo.innerHTML = "";
-
     data = JSON.parse(FooBar.getData()); //mydata
     
     console.log(data);
+    
+    dashboardBeerLevel = document.querySelector(".beer-levels");
+    dashboardBeerLevel.innerHTML = "";
+    
+    dashboardServingInfo = document.querySelector(".serving-info");
+    dashboardServingInfo.innerHTML = "";
+    
+    dashboardStorageInfo = document.querySelector(".storage-info");
+    dashboardStorageInfo.innerHTML = "";
 
-        
+    dashboardQueueInfo = document.querySelector(".queue-info");
+    dashboardQueueInfo.innerHTML = "";
     
+    dashboardBarInfo = document.querySelector(".bar-info");
+    dashboardBarInfo.innerHTML = "";
+  
     
 
-
-    
-    
     //Setting bar data in the template
+    showBarData(data.bar);
     
-    let barData = data.bar;
+    //Setting bartender data in the template
+    showBartenderData(data.bartenders);
+    
+
+    //Setting beer level data in the template
+    showBeerData(data.taps);
+
+    //Setting queue data in the template
+    showQueueData(data.queue);
+
+    
+    //Setting serving data in the template
+    showServingInfo(data.serving);
+
+    //Setting storage data in the template
+    showStorageData(data.storage);
+
+};
+
+function showBarData(barData){
     let barInfoTemplate = document.querySelector("#barInfoTemplate").content;
     let barClone = barInfoTemplate.cloneNode(true);
     let statusBoard = barClone.querySelector(".status-board");
@@ -123,89 +149,10 @@ function gettingData(){
             closesInDiv.textContent = "Closes in "+closesIn+" hours";
         }
         
-    }
+    };
+}
 
-
-
-    
-
-    
-
-
-
-    //Setting beer info data in the template
-    //....
-    
-    //Setting bartender data in the template
-
-    let bartenderData = data.bartenders;
-    bartenderData.forEach(function(bartender){
-        let bartenderInfoTemplate = document.querySelector("#bartenderInfoTemplate").content;
-        let bartenderClone = bartenderInfoTemplate.cloneNode(true);
-        let bartenderName = bartenderClone.querySelector(".bartender-name");
-        let bartenderStatus = bartenderClone.querySelector(".bartender-status");
-        let bartenderPeopleServed = bartenderClone.querySelector(".bartender .people-served");
-        let bartenderImg = bartenderClone.querySelector(".bartender-image");
-
-        bartenderName.textContent = bartender.name;
-
-        if(bartender.status=='WORKING'){
-            bartenderStatus.textContent = "Pouring beer";
-        }
-        if(bartender.status=='READY'){
-            bartenderStatus.textContent = "Chilling";
-        }
-
-        
-        
-        
-        //console.log("Serving customer: "+bartender.servingCustomer);
-
-        if(bartender.name=="Jonas"){
-            if(bartender.servingCustomer==null){
-                console.log("Current customer for Jonas: "+bartender.servingCustomer);
-                console.log("Array of served customers for Jonas: "+b1Customers);
-            }else{
-                b1Customers.push(bartender.servingCustomer);
-                console.log("Current customer for Jonas: "+bartender.servingCustomer);
-                console.log("Array of served customers for Jonas: "+b1Customers);
-            }
-            bartenderPeopleServed.textContent = b1Customers.length+" customers served";
-            bartenderImg.setAttribute("src", "images/1.jpg");
-        }
-        if(bartender.name=="Peter"){
-            if(bartender.servingCustomer==null){
-                console.log("Current customer for Peter: "+bartender.servingCustomer);
-                console.log("Array of served customers for Peter: "+b2Customers);
-            }else{
-                b2Customers.push(bartender.servingCustomer);
-                console.log("Current customer for Peter: "+bartender.servingCustomer);
-                console.log("Array of served customers for Peter: "+b2Customers);
-            }
-            bartenderPeopleServed.textContent = b2Customers.length+" customers served";
-            bartenderImg.setAttribute("src", "images/2.jpg");
-        }
-        if(bartender.name=="Martin"){
-            if(bartender.servingCustomer==null){
-                console.log("Current customer for Martin: "+bartender.servingCustomer);
-                console.log("Array of served customers for Martin: "+b3Customers);
-            }else{
-                b3Customers.push(bartender.servingCustomer);
-                console.log("Current customer for Martin: "+bartender.servingCustomer);
-                console.log("Array of served customers for Martin: "+b3Customers);
-            }
-            bartenderPeopleServed.textContent = b3Customers.length+" customers served";
-            bartenderImg.setAttribute("src", "images/3.jpg");
-        }
-
-        dashboardBartenderInfo.appendChild(bartenderClone);
-    });
-
-    
-
-    //Setting beer level data in the template
-    
-    let beerData = data.taps;
+function showBeerData(beerData){
     beerData.sort(function (a, b) {
         return a.level - b.level;
     });
@@ -247,22 +194,49 @@ function gettingData(){
 
         dashboardBeerLevel.appendChild(beerLevelClone);
     });
+};
 
-    //Setting queue data in the template
-    
-    let queueData = data.queue;
-    //test for serving queue:
+function showServingInfo(servingData){
+    console.log("Serving data:");
+    console.log(servingData);
     let orderLength;
-    
-    // queueData.forEach(function(queue){
-    //     console.log(queue.order.length);
-    //     orderLength = queue.order.length;
-    //     allOrders += orderLength;
-    // });
-    
-    
+    let servingInfoTemplate = document.querySelector("#servingInfoTemplate").content;
+    let servingClone = servingInfoTemplate.cloneNode(true);
+    let beersServed = servingClone.querySelector(".beers-served");
     
 
+    servingData.forEach(function(oneServe){
+        //console.log(oneServe.order.length);
+        
+        
+        if(beersServedIds.includes(oneServe.id)){
+            console.log("When checked if the array has the ids:");
+            console.log(beersServedIds);
+        }else{
+            console.log("In the else statement:");
+            console.log(beersServedIds);
+            beersServedIds.push(oneServe.id);
+            orderLength = oneServe.order.length;
+            allOrders += orderLength;
+            console.log(oneServe.order);
+        }
+        
+    });
+
+    
+    // if(b2Customers.includes(bartender.servingCustomer)){
+    //     console.log("Do nothing");
+    // }else{
+    //     b2Customers.push(bartender.servingCustomer);
+    //     console.log(b2Customers);
+    // }
+
+    beersServed.textContent = allOrders;
+    dashboardServingInfo.appendChild(servingClone);
+}
+
+function showQueueData(queueData){
+    
     let queueInfoTemplate = document.querySelector("#queueInfoTemplate").content;
     let queueClone = queueInfoTemplate.cloneNode(true);
     let peopleInQueue = queueClone.querySelector(".people-in-queue");
@@ -275,35 +249,168 @@ function gettingData(){
         person.style.height = "100px";
         person.style.width = "50px";
 
-        peopleInQueue.style.height=queueData.length*50;
+        peopleInQueue.style.width=queueData.length*48+"px";
 
         queueViz.appendChild(person);
     }
 
     dashboardQueueInfo.appendChild(queueClone);
+};
 
-    
-    //Setting serving data in the template
-    
-    let servingInfoTemplate = document.querySelector("#servingInfoTemplate").content;
-    let servingClone = servingInfoTemplate.cloneNode(true);
-    let servingData = data.serving;
-    let beersServed = servingClone.querySelector(".beers-served");
+function showBartenderData(bartenderData) {
+    let dashboardBartenderInfo = document.querySelector(".bartender-info");
+    dashboardBartenderInfo.innerHTML = "";
 
-    servingData.forEach(function(oneServe){
-        //console.log(oneServe.order.length);
-        orderLength = oneServe.order.length;
-        allOrders += orderLength;
+
+  //  let bartenderData = data.bartenders;
+    bartenderData.forEach(function(bartender){
+        let bartenderInfoTemplate = document.querySelector("#bartenderInfoTemplate").content;
+        let bartenderClone = bartenderInfoTemplate.cloneNode(true);
+        let bartenderName = bartenderClone.querySelector(".bartender-name");
+        let bartenderStatus = bartenderClone.querySelector(".bartender-status");
+        let bartenderPeopleServed = bartenderClone.querySelector(".bartender .people-served");
+        let bartenderImg = bartenderClone.querySelector(".bartender-image");
+
+        bartenderName.textContent = bartender.name;
+
+        if(bartender.status=='WORKING'){
+            bartenderStatus.textContent = "Pouring beer";
+        }
+        if(bartender.status=='READY'){
+            bartenderStatus.textContent = "Chilling";
+        }
+
+        
+        
+        
+        //console.log("Serving customer: "+bartender.servingCustomer);
+
+        if(bartender.name=="Jonas"){
+            if(bartender.servingCustomer==null){
+                console.log("Current customer for Jonas: "+bartender.servingCustomer);
+                console.log("Array of served customers for Jonas: "+b1Customers);
+            }else{
+                // TODO:: only push if bartender.servingCustomer is not in the array!
+                if(b1Customers.includes(bartender.servingCustomer)){
+                    console.log("Do nothing");
+                }else{
+                    b1Customers.push(bartender.servingCustomer);
+                    console.log(b1Customers);
+                }
+                
+                console.log("Current customer for Jonas: "+bartender.servingCustomer);
+                console.log("Array of served customers for Jonas: "+b1Customers);
+            }
+            bartenderPeopleServed.textContent = b1Customers.length+" customers served";
+            bartenderImg.setAttribute("src", "images/1.jpg");
+        }
+        if(bartender.name=="Peter"){
+            if(bartender.servingCustomer==null){
+                console.log("Current customer for Peter: "+bartender.servingCustomer);
+                console.log("Array of served customers for Peter: "+b2Customers);
+            }else{
+                if(b2Customers.includes(bartender.servingCustomer)){
+                    console.log("Do nothing");
+                }else{
+                    b2Customers.push(bartender.servingCustomer);
+                    console.log(b2Customers);
+                }
+                console.log("Current customer for Peter: "+bartender.servingCustomer);
+                console.log("Array of served customers for Peter: "+b2Customers);
+            }
+            bartenderPeopleServed.textContent = b2Customers.length+" customers served";
+            bartenderImg.setAttribute("src", "images/2.jpg");
+        }
+        if(bartender.name=="Martin"){
+            if(bartender.servingCustomer==null){
+                console.log("Current customer for Martin: "+bartender.servingCustomer);
+                console.log("Array of served customers for Martin: "+b3Customers);
+            }else{
+                if(b3Customers.includes(bartender.servingCustomer)){
+                    console.log("Do nothing");
+                }else{
+                    b3Customers.push(bartender.servingCustomer);
+                    console.log(b3Customers);
+                }
+                console.log("Current customer for Martin: "+bartender.servingCustomer);
+                console.log("Array of served customers for Martin: "+b3Customers);
+            }
+            bartenderPeopleServed.textContent = b3Customers.length+" customers served";
+            bartenderImg.setAttribute("src", "images/3.jpg");
+        }
+
+        dashboardBartenderInfo.appendChild(bartenderClone);
     });
 
-    beersServed.textContent = allOrders;
-    dashboardServingInfo.appendChild(servingClone);
+};
 
-    //console.log("Orders in total: "+allOrders);
-    
-    
-    let storageData = data.storage;
+function showBeerTypeData(beertypeData){
+    console.log(beertypeData);
+    beertypeData.forEach(function(beertype){
+        let dashboardBeerInfo = document.querySelector(".beer-info");
 
+        let beerInfoTemplate = document.querySelector("#beerInfoTemplate").content;
+        let beerClone = beerInfoTemplate.cloneNode(true);
+        let beerName = beerClone.querySelector(".beer-desc h2");
+        let beerDescription = beerClone.querySelector(".beer-desc");
+        let beerAroma = beerClone.querySelector(".aroma");
+        let beerAppearance = beerClone.querySelector(".appearance");
+        let beerFlavor = beerClone.querySelector(".flavor");
+        let beerMouthfeel = beerClone.querySelector(".mouthfeel");
+        let beerOverallImpression = beerClone.querySelector(".overall-impression");
+        let beerImg = beerClone.querySelector(".beer-img");
+        let beerButton = beerClone.querySelector(".btn");
+        
+
+        
+        beerImg.setAttribute("src", "images/"+beertype.label);
+        
+        beerName.textContent = beertype.name;
+        beerAroma.textContent = "Aroma: "+beertype.description.aroma;
+        beerAppearance.textContent = "Appearance: "+beertype.description.appearance;
+        beerFlavor.textContent = "Flavor: "+beertype.description.flavor;
+        beerMouthfeel.textContent = "Mouth feel: "+beertype.description.mouthfeel;
+        beerOverallImpression.textContent = "Overall impression: "+beertype.description.overallImpression;
+
+        console.log(beerButton);
+        
+        beerDescription.classList.add("hidden");
+        
+
+        let modal = document.querySelector('.modal');
+        let closeButton = document.querySelector(".close-btn");
+
+        modal.classList.add("hidden");
+
+        function showDetails(data){
+
+            let name = modal.querySelector('.modal-content h1');
+            console.log(data);
+            name.textContent = data.name;
+
+            modal.classList.remove('hidden');
+        };
+
+        
+
+        beerButton.addEventListener('click', function(e){
+            e.preventDefault();
+            showDetails(beertype);
+        });
+
+        closeButton.addEventListener("click", function(){
+            modal.classList.add("hidden");
+        });
+
+        
+        
+        dashboardBeerInfo.appendChild(beerClone);
+        
+
+    });
+}
+
+function showStorageData(storageData){
     storageData.sort(function (a, b) {
         return a.amount - b.amount;
     });
@@ -347,38 +454,7 @@ function gettingData(){
 
         
     });
-
-    let beertypeData = data.beertypes;
-    console.log(beertypeData);
-
-    beertypeData.forEach(function(beertype){
-        let beerInfoTemplate = document.querySelector("#beerInfoTemplate").content;
-        let beerClone = beerInfoTemplate.cloneNode(true);
-        let beerName = beerClone.querySelector(".beer-description h2");
-        let beerAroma = beerClone.querySelector(".aroma");
-        let beerAppearance = beerClone.querySelector(".appearance");
-        let beerFlavor = beerClone.querySelector(".flavor");
-        let beerMouthfeel = beerClone.querySelector(".mouthfeel");
-        let beerOverallImpression = beerClone.querySelector(".overall-impression");
-
-        beerName.textContent = beertype.name;
-        beerAroma.textContent = "Aroma: "+beertype.description.aroma;
-        beerAppearance.textContent = "Appearance: "+beertype.description.appearance;
-        beerFlavor.textContent = "Flavor: "+beertype.description.flavor;
-        beerMouthfeel.textContent = "Mouth feel: "+beertype.description.mouthfeel;
-        beerOverallImpression.textContent = "Overall impression: "+beertype.description.overallImpression;
-
-        dashboardBeerInfo.appendChild(beerClone);
-    });
-    
-    
-    
-
-};
-
-
-
-
+}
 
 
 
